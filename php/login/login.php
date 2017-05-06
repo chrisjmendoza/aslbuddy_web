790e4d362a80f1deb4c208c54a7d5db449b7a189
Chris Mendoza <chrisjmendoza@gmail.com>
2017-05-06 15:36:43 -0700

    updates to login.php to correct mysql query

--- php/login/login.php
+++ php/login/login.php
@@ -15,7 +15,7 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
     $myusername = mysqli_real_escape_string($db, $_POST['username']);
     $mypassword = mysqli_real_escape_string($db, $_POST['password']);
 
-    $sql = "SELECT id FROM user WHERE email = '$myusername' and password = '$mypassword'";
+    $sql = "SELECT id FROM user WHERE email = '$myusername' and passHash = '$mypassword'";
     $result = mysqli_query($db,$sql);
     $row = mysqli_fetch_array($result,MYSQLI_ASSOC);
     $active = $row['active'];
